0c4d17df7ec48ffdbda92d6297a89f5eafed797f
vertiporogov <133884681+vertiporogov@users.noreply.github.com>
2023-07-03 19:54:11 +0300

    finish

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,14 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
 import pytest
-test = Item("ttt", 100.0, 10)
-def test_calculate_total_price():
-    t = test.price
-    v = test.quantity
-    assert t * v == 1000.0
+
+@pytest.fixture
+def test_item():
+    return Item('fff', 100.0, 2)
+def test_item_init(test_item):
+    assert test_item.name == 'fff'
+    assert test_item.price == 100.0
+    assert test_item.quantity == 2
     # assert 100 * 0.1 == 10
 
-# def test_apply_discount():
-#
-#     assert 2000 * 0.8 == 1600
-#     # assert 2000 * 2 == 4000
+def test_apply_discount(test_item):
+    test_item.pay_rate = 0.5
+    test_item.apply_discount()
+    assert test_item.price == 50.0
