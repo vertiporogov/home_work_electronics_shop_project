5d67a769f19e31abae7cc58b58470baa6311be77
vertiporogov <133884681+vertiporogov@users.noreply.github.com>
2023-07-03 19:34:05 +0300

    change tyter

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,13 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-
+from src.item import Item
 import pytest
-
+test = Item("ttt", 100.0, 10)
 def test_calculate_total_price():
-
-    assert 100 * 10 == 1000
+    t = test.price
+    v = test.quantity
+    assert t * v == 1000.0
     # assert 100 * 0.1 == 10
 
-def test_apply_discount():
-
-    assert 2000 * 0.8 == 1600
-    # assert 2000 * 2 == 4000
+# def test_apply_discount():
+#
+#     assert 2000 * 0.8 == 1600
+#     # assert 2000 * 2 == 4000
