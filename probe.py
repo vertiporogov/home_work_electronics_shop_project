248dce909e57d01e48f38476802d1f11f8dc028a
vertiporogov <133884681+vertiporogov@users.noreply.github.com>
2023-10-05 19:09:04 +0300

    fixed PEP 2 finish

--- probe.py
+++
@@ -1,8 +0,0 @@
-import csv
-ggg = []
-with open('src/items.csv', encoding='utf-8') as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        ggg.append(row)
-
-    print(ggg)
--- tests/test_item.py
+++ tests/test_item.py
@@ -2,27 +2,34 @@
 from src.item import Item
 import pytest
 
+
 @pytest.fixture
 def test_item():
     return Item('fff', 100.0, 2)
+
+
 def test_item_init(test_item):
     assert test_item.name == 'fff'
     assert test_item.price == 100.0
     assert test_item.quantity == 2
     # assert 100 * 0.1 == 10
 
+
 def test_apply_discount(test_item):
     test_item.pay_rate = 0.5
     test_item.apply_discount()
     assert test_item.price == 50.0
 
+
 def test_calculate_total_price(test_item):
     assert test_item.calculate_total_price() == 200.0
+
 
 def test_string_to_number(test_item):
     assert type(test_item.string_to_number('3')) == int
     assert type(test_item.string_to_number('3.0')) == int
     assert type(test_item.string_to_number('3.3')) == int
+
 
 def test_instantiate_from_csv(test_item):
     test_item.instantiate_from_csv()
